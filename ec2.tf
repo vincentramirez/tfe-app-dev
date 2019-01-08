6dfded85ca2a8d9e7ebebb00e84b74a37aa394a5
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2019-01-08 12:21:42 -0600

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -4,7 +4,7 @@ module "ec2_instance" {
   source  = "app.terraform.io/vinnie-demo/ec2-instance/aws"
   version = "1.13.0"
 
-  ami = "ami-0b59bfac6be064b78"
+  ami = "ami-009d6802948d06e52"
   associate_public_ip_address = "false"
   instance_type = "t2.micro"
   name = "vinnie-mod-test"
